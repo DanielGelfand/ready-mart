49f6db41b317035784fe1ae4c44f4aaa58e216cd
Amit Narang <anarang1@stuy.edu>
2018-10-18 13:16:25 -0400

    Devlog, others

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
-from util import
+from flask import Flask, request, render_template, session, redirect, url_for
+from os import urandom
 
 app = Flask(__name__)
+# sets the secret_key to a random string of bytes
+app.secret_key = urandom(32
 
 def __init__(self):
 
 @app.route('/')
 def home():
+    if "username" not in session.keys():
+        return redirect(url_for("login"))
+    return render_template("")
+
+@app.route("/login")
+def login():
