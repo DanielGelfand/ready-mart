76d5ec0e2e1e7824056301b88edcd6bf9e0eae13
Amit Narang <anarang1@stuy.edu>
2018-10-19 13:13:14 -0400

    Amit

--- app.py
+++ app.py
@@ -3,15 +3,28 @@ from os import urandom
 
 app = Flask(__name__)
 # sets the secret_key to a random string of bytes
-app.secret_key = urandom(32
 
-def __init__(self):
-
-@app.route('/')
+@app.route("/")
 def home():
     if "username" not in session.keys():
         return redirect(url_for("login"))
-    return render_template("")
+    return render_template("add_story.html")
 
-@app.route("/login")
+@app.route("/login", methods=["POST", "GET"])
 def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def new():
+    return render_template("add_story.html")
+
+@app.route("/PreviousWork")
+
+@app.route("/NewStory")
+
+@app.route("/AddToStory")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
