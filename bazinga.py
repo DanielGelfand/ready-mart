e6cc3dcadabd2fef2d756a5b7f4a6e082c18d598
Theodore Peters <tpeters@stuy.edu>
2018-10-20 19:04:12 -0400

    more things

--- bazinga.py
+++ bazinga.py
@@ -2,25 +2,51 @@ import sqlite3
 
 # Come to the computer interaction club. #
 
+def reset(squul):
+    squul.execute("DROP TABLE IF EXISTS stories;")
+    squul.execute("DROP TABLE IF EXISTS history;")
+    squul.execute("DROP TABLE IF EXISTS users;")
+
+    squul.execute("CREATE TABLE stories (storyid INTEGER PRIMARY KEY, content TEXT, lastedit TEXT);")
+    squul.execute("CREATE TABLE history (userid INTEGER PRIMARY KEY);")
+    squul.execute("CREATE TABLE users (userid INTEGER PRIMARY KEY, username TEXT, password TEXT);")
+
 def last_story(squul, storyid):
-    squul.execute("SELECT lastEdit FROM stories WHERE stories.StoryId = ?;", (storyid, ))
+    squul.execute("SELECT lastedit FROM stories WHERE stories.storyid = ?;", (storyid, ))
     return squul.fetchall()[0][0]
 
 def hole_story(squul, storyid):
-    squul.execute("SELECT content, lastEdit FROM stories WHERE stories.StoryId = ?;", (storyid, ))
-    return '\n'.join(squul.fetchall()[0])
+    squul.execute("SELECT content, lastedit FROM stories WHERE stories.storyid = ?;", (storyid, ))
+    return '\n'.join(str(i) for i in squul.fetchall()[0] if i != None)
 
 def edit_story(squul, storyid, shrext, userid):
-    squul.execute("UPDATE stories SET content = ? WHERE stories.StoryId = ?;", (hole_story(squul, storyid), storyid))
-    squul.execute("UPDATE stories SET lastEdit = ? WHERE stories.StoryId = ?;", (shrext, storyid))
-    squul.execute("UPDATE history SET {} = '.' WHERE history.userId = {};".format('u' + str(storyid), userid))
+    if can_edit(squul, storyid, userid):
+        squul.execute("UPDATE stories SET content = ? WHERE stories.storyid = ?;", (hole_story(squul, storyid), storyid))
+        squul.execute("UPDATE stories SET lastedit = ? WHERE stories.storyid = ?;", (shrext, storyid))
+        squul.execute("UPDATE history SET {} = '.' WHERE history.userid = {};".format('s' + str(storyid), userid))
+        
+def can_edit(squul, storyid, userid):
+    squul.execute("SELECT {} FROM history WHERE history.userid = {};".format('s' + str(storyid), userid))
+    return squul.fetchall()[0][0] == 0
 
+def add_story(squul, storyid, shrext, userid):
+    squul.execute("INSERT INTO stories VALUES(?, ?, ?);", (storyid, None, shrext))
+    squul.execute("ALTER TABLE history ADD COLUMN {} INTEGER DEFAULT 0;".format('s' + str(storyid)))
+    squul.execute("UPDATE history SET {} = 1 WHERE history.userid = {};".format('s' + str(storyid), userid))
 
+def add_user(squul, userid, username, hashword):
+    squul.execute("INSERT INTO users VALUES(?, ?, ?);", (userid, username, hashword))
+    squul.execute("INSERT INTO history (userid) VALUES(?);", (userid,));
+    
 if __name__ == "__main__":
     db = sqlite3.connect('stories.db')
     c = db.cursor()
-    last_story(c, 4321)
-    hole_story(c, 4321)
-    edit_story(c, 4321, "wow the computer interaction club is so epic", 1877)
+    reset(c)
+    add_user(c, 1, "Mr. Kats", "qwerty")
+    add_story(c, 1, "hello does this website work",  1)
+    edit_story(c, 1, "wait i forgot to actually write a story", 1)
+    add_user(c, 2, "Mr. Kats' alt account", "qwertzu")
+    edit_story(c, 1, "i will circumvent this story editing restriction with an alt account", 2)
+    print(hole_story(c, 1))
     db.commit()
     db.close()
