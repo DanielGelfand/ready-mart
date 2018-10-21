605f68c8ab137116c4955844bef518c121dbbcec
Ray <rayonishi11@gmail.com>
2018-10-20 23:49:06 -0400

    Users can login and signup, bazinga moved to utils, templates added

--- util/bazinga.py
+++ util/bazinga.py
@@ -24,7 +24,7 @@ def edit_story(squul, storyid, shrext, userid):
         squul.execute("UPDATE stories SET content = ? WHERE stories.storyid = ?;", (hole_story(squul, storyid), storyid))
         squul.execute("UPDATE stories SET lastedit = ? WHERE stories.storyid = ?;", (shrext, storyid))
         squul.execute("UPDATE history SET {} = '.' WHERE history.userid = {};".format('s' + str(storyid), userid))
-        
+
 def can_edit(squul, storyid, userid):
     squul.execute("SELECT {} FROM history WHERE history.userid = {};".format('s' + str(storyid), userid))
     return squul.fetchall()[0][0] == 0
@@ -37,16 +37,25 @@ def add_story(squul, storyid, shrext, userid):
 def add_user(squul, userid, username, hashword):
     squul.execute("INSERT INTO users VALUES(?, ?, ?);", (userid, username, hashword))
     squul.execute("INSERT INTO history (userid) VALUES(?);", (userid,));
-    
-if __name__ == "__main__":
-    db = sqlite3.connect('stories.db')
-    c = db.cursor()
-    reset(c)
-    add_user(c, 1, "Mr. Kats", "qwerty")
-    add_story(c, 1, "hello does this website work",  1)
-    edit_story(c, 1, "wait i forgot to actually write a story", 1)
-    add_user(c, 2, "Mr. Kats' alt account", "qwertzu")
-    edit_story(c, 1, "i will circumvent this story editing restriction with an alt account", 2)
-    print(hole_story(c, 1))
-    db.commit()
-    db.close()
+
+#Checks whether a user exists in the database
+def user_exists(squul,user):
+    exist = squul.execute("SELECT EXISTS(SELECT 1 FROM users WHERE username = ?);", (user,))
+    return exist.fetchone()[0] == 1
+
+#Checks the user credentials
+def check_user(squul,user,pword):
+    toCheck = squul.execute("SELECT password from users WHERE username = ?;",(user,))
+    return toCheck.fetchone()[0] == pword
+
+# if __name__ == "__main__":
+#     db = sqlite3.connect('stories.db')
+#     c = db.cursor()
+#     add_user(c, 1, "Mr. Kats", "qwerty")
+#     add_story(c, 1, "hello does this website work",  1)
+#     edit_story(c, 1, "wait i forgot to actually write a story", 1)
+#     add_user(c, 2, "Mr. Kats' alt account", "qwertzu")
+#     edit_story(c, 1, "i will circumvent this story editing restriction with an alt account", 2)
+#     print(hole_story(c, 1))
+#     db.commit()
+#     db.close()
