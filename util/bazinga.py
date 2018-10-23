cbb1f6920d739b98fbce789494989e200f1450d2
Theodore Peters <tpeters@stuy.edu>
2018-10-22 20:46:39 -0400

    comments & commits

--- util/bazinga.py
+++ util/bazinga.py
@@ -2,6 +2,8 @@ import sqlite3
 
 # Come to the computer interaction club. #
 #squul is cursor
+
+# clears database of all data, rebuilds tables
 def reset(squul):
     squul.execute("DROP TABLE IF EXISTS stories;")
     squul.execute("DROP TABLE IF EXISTS history;")
@@ -11,13 +13,21 @@ def reset(squul):
     squul.execute("CREATE TABLE history (userid INTEGER PRIMARY KEY);")
     squul.execute("CREATE TABLE users (userid INTEGER PRIMARY KEY, username TEXT, password TEXT);")
 
+# last edit of story
 def last_story(squul, storyid):
     squul.execute("SELECT lastedit FROM stories WHERE stories.storyid = ?;", (storyid, ))
     return squul.fetchall()[0][0]
 
+# entiretity of story; what would be displayed to a user who edited said story
 def hole_story(squul, storyid):
     squul.execute("SELECT content, lastedit FROM stories WHERE stories.storyid = ?;", (storyid, ))
     return '\n'.join(str(i) for i in squul.fetchall()[0] if i != None)
+
+
+# updates last edit and content of story to reflect edit.
+# also updates history to show user has edited story
+
+# TODO: parse the story text for html/sql trickery
 
 def edit_story(squul, storyid, shrext, userid):
     if can_edit(squul, storyid, userid):
@@ -25,28 +35,38 @@ def edit_story(squul, storyid, shrext, userid):
         squul.execute("UPDATE stories SET lastedit = ? WHERE stories.storyid = ?;", (shrext, storyid))
         squul.execute("UPDATE history SET {} = '.' WHERE history.userid = {};".format('s' + str(storyid), userid))
 
+
+# returns whether a user should be in editing mode for a particular story
+# decide whether to display editing mode of story, or which stories to list on users home page.
 def can_edit(squul, storyid, userid):
     squul.execute("SELECT {} FROM history WHERE history.userid = {};".format('s' + str(storyid), userid))
     return squul.fetchall()[0][0] == 0
 
-def add_story(squul, storyid, shrext, userid,title):
+# creates a new story info in stories and history table
+
+# TODO: currently, people editing story will be unable to see title -- is this our prefered functionality??
+# could keep as is, or add seperate title field to display story title.
+def add_story(squul, storyid, shrext, userid, title):
     squul.execute("INSERT INTO stories VALUES(?, ?, ?);", (storyid, title, shrext))
     squul.execute("ALTER TABLE history ADD COLUMN {} INTEGER DEFAULT 0;".format('s' + str(storyid)))
     squul.execute("UPDATE history SET {} = 1 WHERE history.userid = {};".format('s' + str(storyid), userid))
 
+# creates entries for a new user, both for user and in history database
 def add_user(squul, userid, username, hashword):
     squul.execute("INSERT INTO users VALUES(?, ?, ?);", (userid, username, hashword))
     squul.execute("INSERT INTO history (userid) VALUES(?);", (userid,));
 
-#Checks whether a user exists in the database
-def user_exists(squul,user):
+# validates a username exists
+def user_exists(squul, user):
     exist = squul.execute("SELECT EXISTS(SELECT 1 FROM users WHERE username = ?);", (user,))
     return exist.fetchone()[0] == 1
 
-#Checks the user credentials
-def check_user(squul,user,pword):
-    toCheck = squul.execute("SELECT password from users WHERE username = ?;",(user,))
+# checks if user's password valid
+def check_user(squul, user, pword):
+    toCheck = squul.execute("SELECT password from users WHERE username = ?;", (user,))
     return toCheck.fetchone()[0] == pword
+
+# old unit tests:
 
 # if __name__ == "__main__":
 #     db = sqlite3.connect('stories.db')
