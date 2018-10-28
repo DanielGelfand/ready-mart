6de12cc526d7cc64ee834026e29208cee2503f53
Ray <rayonishi11@gmail.com>
2018-10-28 00:24:43 -0400

    Added comments and made some minor fixes

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ app.secret_key = urandom(32)
 
 @app.route("/")
 def home():
+    #if the user is logged in, redirect them to userHome
     if "user" in session:
         return redirect(url_for('userHome'))
+    #otherwise redirect them to the landing page
     return render_template("landing.html")
 
 @app.route("/login",methods=["GET","POST"])
@@ -33,9 +35,12 @@ def login():
         else:
             #case for bad username
             error="bad username"
+    #checks if the user is logged in already and tries to access the page
     if "user" in session:
+        #flashes an error if they try to do so then redirects to userHome
         flash("You tried to access the login page while logged in! If you wish to log in to another account, log out first!")
         return redirect(url_for('userHome'))
+    #displays the error the user faced when logging in
     flash(error)
     return render_template("login.html")
 
@@ -66,22 +71,29 @@ def register():
             db.close()
             #redirects user to home
             return redirect(url_for('userHome'))
+    #checks if the user is logged in already and tries to access the page
     if "user" in session:
+        #flashes an error if they try to do so then redirects to userHome
         flash("You tried to access the register page while logged in! If you wish to create a new account, log out first!")
         return redirect(url_for('userHome'))
+    #displays the error the user faced when registering an account
     flash(error)
     return render_template("register.html")
 
 
 @app.route("/userHome")
 def userHome():
+    #checks if the user is not logged in
     if "user" not in session:
+        #flashes the error messsage below if they are not logged in
         flash("You tried to access the user home page without being logged in! If you wish to access this feature, log in or register an account first!")
         return redirect(url_for('home'))
     db = sqlite3.connect("stories.db")
     c = db.cursor()
-    id = userID = c.execute("SELECT userid FROM users WHERE username = ?",(session["user"],)).fetchone()[0]
-    stories = dbtools.not_edit(c,id)
+    #gets the userid of the user in session
+    userID = c.execute("SELECT userid FROM users WHERE username = ?",(session["user"],)).fetchone()[0]
+    #fetches all the stories that the user has contributed to
+    stories = dbtools.not_edit(c,userID)
     return render_template("userHome.html",username = session["user"],storyList = stories)
 
 @app.route("/add",methods=["GET","POST"])
@@ -91,25 +103,39 @@ def add():
         contrib = request.form["content"]
         db = sqlite3.connect('stories.db')
         c = db.cursor()
+        #fetches id of most recently added story
         forID = c.execute("SELECT storyid FROM stories ORDER BY storyid DESC LIMIT 1")
         id = forID.fetchone()[0]
+        #fetches user id of current user
         currID = c.execute("SELECT userid FROM users WHERE username = ?",(session["user"],)).fetchone()[0]
+        #adds story to databae
         dbtools.add_story(c,id+1,contrib,currID,title)
         db.commit()
         db.close()
+        #message to let user know that the story was successfully created
         flash("Successfully created new story")
         return redirect(url_for('userHome'))
+    #checks if the user is not logged in
     if "user" not in session:
+        #flashes the error messsage below if they are not logged in
         flash("You tried to access the add page without being logged in! If you wish to add a new story, log in or register an account first!")
         return redirect(url_for('home'))
     return render_template("add_story.html")
 
 @app.route("/choose")
 def edit():
+    #checks if the user is not logged in
+    if "user" not in session:
+        #flashes the error messsage below if they are not logged in
+        flash("You tried to access the edit page without being logged in! If you wish to access this feature, log in or register an account first!")
+        return redirect(url_for('home'))
     db = sqlite3.connect("stories.db")
     c = db.cursor()
-    id = userID = c.execute("SELECT userid FROM users WHERE username = ?",(session["user"],)).fetchone()[0]
-    stories = dbtools.all_edit(c,id)
+    #gets userid of current user in session
+    userID = c.execute("SELECT userid FROM users WHERE username = ?",(session["user"],)).fetchone()[0]
+    #fetches all the stories the user can edit
+    stories = dbtools.all_edit(c,userID)
+    #renders template using the stories the user can edit
     return render_template("edit_stories.html", storyList = stories)
 
 
@@ -121,16 +147,22 @@ def edit_story():
         storyID = request.form["storyID"]
         title = dbtools.title_story(c,storyID)
         content = dbtools.last_story(c,storyID)
+        #acquires the contribution to be made to the story
         contrib = request.form["content"]
+        #fetches the userid of current user in session
         userID = c.execute("SELECT userid FROM users WHERE username = ?",(session["user"],)).fetchone()[0]
+        #edits story
         dbtools.edit_story(c,storyID,contrib,userID)
         db.commit()
         db.close()
         flash("Successfully added to story")
         return redirect(url_for('userHome'))
+    #checks if the user is not logged in
     if "user" not in session:
+        #flashes error messsage below if they are not logged in
         flash("You tried to access the edit page without being logged in! If you wish to access this feature, log in or register an account first!")
         return redirect(url_for('home'))
+    #in case of a user typing localhost:5000/edit, displays the error below
     flash("You tried to access /edit. If you wish to edit a story, please click on Add to an existing story if you wish to add to a storym")
     return redirect(url_for('userHome'))
 
@@ -140,26 +172,46 @@ def editPage():
     c = db.cursor()
     if request.method == "POST":
         storyID = request.form["storyID"]
+        #fetches the title of the story using title_story
         storyTitle = dbtools.title_story(c,storyID)
+        #fetches the last made contribution using last_story
         prevContrib = dbtools.last_story(c,storyID)
+        #renders template using the title and last made contribution
         return render_template("edit_story.html",title=storyTitle,contrib = prevContrib,id=storyID)
+    #checks if the user is not logged in
     if "user" not in session:
+        #flashes error messsage below if they are not logged in
         flash("You tried to access the edit page without being logged in! If you wish to access this feature, log in or register an account first!")
         return redirect(url_for('home'))
+    #in case of a user typing localhost:5000/editPage, displays the error below
     flash("You tried to access edit page directly, if you wish to edit a story please do so through the home page")
     return redirect(url_for('userHome'))
 
 @app.route("/view/<int:storyid>")
 def view(storyid):
+    #checks if the user is not logged in
+    if "user" not in session:
+        #flashes the error messsage below if they are not logged in
+        flash("You tried to view a story without being logged in! If you wish to access this feature, log in or register an account first!")
+        return redirect(url_for('home'))
     db = sqlite3.connect("stories.db")
     c = db.cursor()
+    userID = c.execute("SELECT userid FROM users WHERE username = ?",(session["user"],)).fetchone()[0]
+    #fetches the entire story using hole_story
     wholeStory = dbtools.hole_story(c,storyid)
+    #fetches the title of the story using title_story
     titleStory = dbtools.title_story(c,storyid)
+    #in case of a user typing localhost:5000/view/storyID, checks if they can edit and if so, redirect them back to home page
+    if dbtools.can_edit(c,storyid,userID):
+        flash("You must contribute to the story titled " + titleStory + " before you can view it")
+        return redirect(url_for('userHome'))
     return render_template("view_story.html",story = wholeStory,title = titleStory)
 
 @app.route("/logout")
 def logout():
+    #removes the user from the session
     session.pop("user")
+    #redirects back to the home page
     return redirect(url_for('home'))
 
 if __name__ == "__main__":
